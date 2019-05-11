8663ba01fb15a62e2533e90dc3ec35378ea16227
Henrikohlsen <Henros18@uia.no>
2019-05-11 19:06:38 +0200

    removed my own api

--- go-speak/main_wit.go
+++ go-speak/main_wit.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/Henrikohlsen/Is-105/go-speak/Wit"
+	"github.com/Henrikohlsen/Is-105/is105-ica04/go-speak/Wit"
 )
 
 func main(){
-	speech.SetWitKey("AGBTMHDDRJUD7FBTFSY3MJLTJS4SEY73")
+	speech.SetWitKey(UseYourWitKeyHere)
 	print := speech.SendWitVoice("test.wav")
 	fmt.Println("The text from the audio file: ", print)
 }
